d35e21eb13d1e3f325247284256e3a15f816c5d2
mouradaitelouhab <mouradprotech@gmail.com>
2025-07-17 20:55:00 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
+
+// vite.config.ts
+{
+  plugins: [
+    // ...
+    chunkSplitPlugin()
+  ]
+}
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
@@ -16,9 +25,9 @@ export default defineConfig({
     port: 5174,
     allowedHosts: [
       'localhost',
-      '5174-iu5cikd8m359izkdqq3cv-8eb1bbb7.manusvm.computer',
-      '5174-i1y5im51on47sijiv6r8a-5d94768d.manusvm.computer',
-      '5175-i1y5im51on47sijiv6r8a-5d94768d.manusvm.computer'
+      '',
+      '',
+      ''
     ]
   }
 })
